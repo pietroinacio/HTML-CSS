99e12d3ca8ccf79a01558767bfb1230ecc0db65a
Pietro Pereira Inacio <141683277+pietroinacio@users.noreply.github.com>
2024-06-08 12:11:03 -0300

    Conteúdos excluídos

--- exercicios/teste/script.js
+++
@@ -1,7 +0,0 @@
-const solLua = document.querySelector('.sol-lua')
-
-document.querySelector('.botao-tema').addEventListener('click', () => {
-    document.body.classList.toggle('escuro')
-    const atualRotacao = parseInt(getComputedStyle(solLua).getPropertyValue('--rotacao'))
-    solLua.style.setProperty('--rotacao', atualRotacao + 180)
-})
